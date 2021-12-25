66d124f1b0582cacf7b0fc4c83ad39ce940410f7
krobingit <liorobin5@gmail.com>
2021-12-25 15:26:11 +0530

    order

--- src/Pages/Orders.jsx
+++ src/Pages/Orders.jsx
@@ -8,7 +8,7 @@ import { commonRequest } from '../axiosreq';
 import Empty  from '../Assets/orderEmpty.png';
 import { Button } from 'semantic-ui-react';
 import { useHistory } from 'react-router-dom';
-import { medium } from '../responsive';
+import { small,medium } from '../responsive';
 
 const LoaderContainer = styled.div`
 display:flex;
@@ -17,6 +17,11 @@ align-items:center;
 justify-content:center;
 `
 const Container = styled.div`
+margin-bottom:1rem;
+`
+const MainContainer = styled.div`
+`
+const Large = styled.div`
 `
 const EmptyContainer = styled.div`
 display:flex;
@@ -45,7 +50,10 @@ border: 1px solid rgba( 255, 255, 255, 0.18 );
 display:flex;
 flex-direction:column;
 flex-wrap:wrap;
+padding:1rem;
 margin-bottom:2rem;
+justify-content:space-around;
+${medium({ alignItems:'center'})}
 `
 
 const ProductCard = styled.div`
@@ -55,8 +63,7 @@ display:flex;
 gap:1.2rem;
 align-items:center;
 justify-content:space-evenly;
-${medium({ flexDirection: "column", width: "22rem" })}
-
+${medium({ flexDirection: "column", justifyContent:"center",width: "22rem" })}
 `
 
 const ProductImage = styled.img`
@@ -70,9 +77,35 @@ const ProductName = styled.h3`
 width:15rem;
 font-size:1.2rem;
 `
+const Line = styled.div`
+  border: 1px dashed darkgray;
+width:100%;
+  margin-bottom: 0.5rem;
+`;
+const Status = styled.span`
+
+`
+const OrderDetail = styled.div`
+font-family: 'Fira Sans', sans-serif;
+background:wheat;
+font-weight:700;
+text-transform:uppercase;
+width:max-content;
+border-radius:1rem;
+padding:0.3rem;
+font-size:1.2rem;
+`
+
+
 const ProductQty = styled.h3``
 const ProductPrice=styled.h3``
-
+const Details = styled.div`
+display:flex;
+margin-bottom:3rem;
+align-items:center;
+gap:2rem;
+${small({flexDirection:"column",gap:"0.5rem"})};
+`
 
 export const Orders = () => {
  let history = useHistory();
@@ -86,6 +119,7 @@ export const Orders = () => {
      headers: { token: currentUser.token }
     })
     setOrder(response.data);
+
     setLoading(false)
    }
    catch (err) {
@@ -93,55 +127,69 @@ export const Orders = () => {
     setLoading(false)
    }
   }
-  fetchData();
- }, [currentUser._id,currentUser.token]);
- console.log(order);
+   fetchData();
+ }, [currentUser]);
+// const products = order.map((each_order) => each_order.products)
+
  return (
-  <>
+  <MainContainer>
   <Navbar />
    <Title><i className="fas fa-shopping-bag"></i> Orders</Title>
    {loading ?
     <LoaderContainer>
-    <SpinnerCircularFixed size={70} thickness={80} speed={163} color="#141e30" secondaryColor="gold" />
-   </LoaderContainer>
+     <SpinnerCircularFixed size={70} thickness={80} speed={163} color="#141e30" secondaryColor="gold" />
+    </LoaderContainer>
     :
-    <Container>{
+    <Container>
+     {
      order.length === 0
-      ?<>
+      ?
+      <>
        <Button
-        style={{ marginLeft: "3rem",marginBottom:"3rem" }}
+        style={{ marginLeft: "3rem", marginBottom: "3rem" }}
         color="yellow"
         onClick={() => history.push("/products")}
-      >
+       >
         Continue to Shop
-      </Button>
-      <EmptyContainer>
+       </Button>
+       <EmptyContainer>
 
-       <h3 style={{fontSize:"1.5rem"}}>You haven't ordered any items yet!</h3>
-             <OrderEmptyImage src={Empty}></OrderEmptyImage>
+        <h3 style={{ fontSize: "1.5rem" }}>You haven't ordered any items yet!</h3>
+        <OrderEmptyImage src={Empty}></OrderEmptyImage>
        </EmptyContainer>
-       </>
+      </>
       :
+      <Summary>
+       <Heading>Orders</Heading>
+       <Products>
+        {order.map((each) =>
+         <Large>
+          <Heading style={{fontSize:"1.2rem",fontWeight:"bold"}}>Order-Id: {each.paymentData.order_id}</Heading>
+          {each.products.map((product) =>
+           <ProductCard key={product._id}>
+            <ProductImage src={product.image} />
+            <ProductName>{product.name}</ProductName>
+            <ProductPrice>₹{Math.round(product.price * 76).toLocaleString()}</ProductPrice>
+            <ProductQty>Qty: {product.quantity}</ProductQty>
+           </ProductCard>)
+          }
+          <Details>
+          <OrderDetail>Order Status: <Status style={{ color: each.orderStatus === "Processing" ? "orange" : "green" }}>{each.orderStatus}</Status></OrderDetail>
+           <OrderDetail>Amount: {(each.paymentData.amount / 100).toLocaleString()}</OrderDetail>
+           <OrderDetail>💳 Paid via Card ending {each.paymentData.card.last4}</OrderDetail>
+           <OrderDetail>#️⃣ Payment ID: {each.paymentData.id}</OrderDetail>
+           <OrderDetail>📱 Deliverd to: {each.paymentData.contact}</OrderDetail>
+          </Details>
+          <Line></Line>
+         </Large>
 
-         <Summary>
- <Heading>Order Summary</Heading>
-           <Products>
-  {order.products.map((product) =>
-    <ProductCard key={product._id}>
-        <ProductImage src={product.image} />
-        <ProductName>{product.name}</ProductName>
-      <ProductPrice>₹{Math.round(product.price*76).toLocaleString()}</ProductPrice>
-      <ProductQty>Qty: {product.quantity}</ProductQty>
-          </ProductCard>
+
         )}
-           </Products>
-           </Summary>
-
-
+       </Products>
+      </Summary>
     }
-
-     </Container>
+    </Container>
    }
-</>
+</MainContainer>
 )
 }
