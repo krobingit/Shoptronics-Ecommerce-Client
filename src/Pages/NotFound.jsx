199a542df9df5618e72c6aa287742ecd467a4a24
krobingit <liorobin5@gmail.com>
2021-12-30 14:08:58 +0530

    ch

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import notfound from '../Assets/404.png';
 import { Button } from 'semantic-ui-react';
-
+import { useHistory } from 'react-router-dom';
 const Container = styled.div`
 min-height:100vh;
 position:relative;
@@ -18,11 +18,11 @@ bottom:2rem;
 left:2rem;
 `
 export const NotFound = () => {
-
+ let history = useHistory();
  return (
   <Container>
    <NotFoundImage src={notfound} />
-   <Btn color="black" >Go to Shoptronics Home</Btn>
+   <Btn color="black" onClick={()=>history.push("/")}>Go to Shoptronics Home</Btn>
    </Container>
 
 )
