48b6d2c649a2eba2b6c5829977b9a77516df7ec9
krobingit <liorobin5@gmail.com>
2023-07-09 01:44:53 +0530

    console.log

--- src/Actions/login_actions.js
+++ src/Actions/login_actions.js
@@ -5,7 +5,7 @@ export const login = async (dispatch, user) =>
 dispatch({type:"loginInitiate"})
  try {
   const response = await commonRequest.post("/userauth/login", user).catch((error)=>{
-    console.log(error.request)
+    console.log(error.response)
    const errorMessage=error.request.statusText || error.message
    console.log(errorMessage)
    if(errorMessage)
